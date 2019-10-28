afa69393e983d140ca8d78fd6642c7130491bd4d
andreadamson <andre.adamson@khk.ee>
2019-10-28 11:08:00 +0200

    break

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ lause = "arv = ";
      lause = lause + " - paaris";
  } else {
     lause = lause + " - paaritu";
-    continue;
+    
 }
 
  console.log(lause)
+ if (arv >= 5) {
+    break;
+ }
 }
