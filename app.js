27c6adf2389149d12576070b3870afc8e848db1d
andreadamson <55825764+andreadamson@users.noreply.github.com>
2019-11-17 16:53:02 +0200

    Paarsus ja väljastus eraldi funktisoonidega

--- app.js
+++ app.js
@@ -2,13 +2,22 @@
 // Funktsioon, mis arvutab ristküliku pindala ja väljastab ekraanil
 
 function ristkylikuPindala(pikkus, laius){
+ // pindala arvutamine
+  let pindala = pikkus * laius;
+  // tagastame pindala põhiprogrammi
+  return pindala;
+}
+
+// Funktsioon, mis väljastab tulemuse ekraanil
+function valjasta(pikkus, laius){
   // väljastame väärtused
   console.log('Pikkus = ' + pikkus);
   console.log('Laius = ' + laius);
-  // arvutame pindala
-  let pindala = pikkus * laius;
-  // väljastame pindala
+  // salvestame arvutatud pindala
+  let pindala = ristkylikuPindala(pikkus, laius);
+  // väljastame tulemus
   console.log('Pindala = ' + pindala);
-}
+}  
 
-ristkylikuPindala(15, 5);
+valjasta(15, 5);
+valjasta(1, 5); 
