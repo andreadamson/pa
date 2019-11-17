bce2be630796675dea9aaf3369f6190e4335c945
andreadamson <55825764+andreadamson@users.noreply.github.com>
2019-11-17 16:57:55 +0200

    Muutujate scope - globaalne

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-// Funktsioonid
-// Funktsioon, mis arvutab ristküliku pindala ja väljastab ekraanil
+// Nähtavus - Scope
+// globaalne
+var a = 1;
+let b = 2;
+const c = 3;
 
-function ristkylikuPindala(pikkus, laius){
- // pindala arvutamine
-  let pindala = pikkus * laius;
-  // tagastame pindala põhiprogrammi
-  return pindala;
-}
-
-// Funktsioon, mis väljastab tulemuse ekraanil
-function valjasta(pikkus = 0, laius = 0){
-  // väljastame väärtused
-  console.log('Pikkus = ' + pikkus);
-  console.log('Laius = ' + laius);
-  // salvestame arvutatud pindala
-  let pindala = ristkylikuPindala(pikkus, laius);
-  // väljastame tulemuse
-  console.log('Pindala = ' + pindala);
-}  
-
-valjasta(15, 5);
-valjasta(1, 5);
-valjasta(); 
+console.log('Globaalne: ', a, b, c);
