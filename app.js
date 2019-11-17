212bab8a78dcdd5a10572fcb16bb55972dfe6f71
andreadamson <55825764+andreadamson@users.noreply.github.com>
2019-11-17 16:39:04 +0200

    Paarisuse kontroll funktisooni abil

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // Funktsioonid
-// arvu paarsuse kontroll
-// suvaline täisarv
-let arv = 5;
-// arvutame jääk 2-ga jagamisel
-let jaak = arv % 2;
-// kui jääk on võrdne nulliga
-if( jaak === 0 ){
-//paaris arv
-console.log(arv + ' on paaris');
-//paaritu arvu
-console.log(arv + ' on paaritu');
+// arvu paarisuse kontroll
+// funktsiooni defineerimine
+
+function paarisuseKontroll(arv){
+  // arvutame jääk 2-ga jagamisel
+  let jaak = arv % 2;
+  // kui jaak on võrdne nulliga
+  if( jaak === 0 ){
+    // tegu on paaris avuga
+    console.log(arv + ' on paaris');
+  } else {
+    // tegu on paaritu arvuga
+    console.log(arv + ' on paaritu');
+  }
 }
+// funktsiooni kasutamine
+paarisuseKontroll(4);
+paarisuseKontroll(9);
